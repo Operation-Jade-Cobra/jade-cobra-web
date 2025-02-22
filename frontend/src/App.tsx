9e70557f47ba7ae218422b0cc6137c7aa5250d22
Lucy Lu <lu.2623@osu.edu>
2025-02-22 14:15:04 -0500

    update orders, orderdetails

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./Home/Home";
 import Products from "./Products/Products";
+import Orders from './Orders/Orders';
+import OrderDetails from './Orders/OrderDetails';
 
 function App() {
   const openMenu = () => {
@@ -30,6 +32,7 @@ function App() {
             <div className="header-links">
                 <Link to="/">Home</Link>
                 <Link to="/catalog">Catalog</Link>
+                <Link to="/orders">Orders</Link>
             </div>
         </header>
 
@@ -51,6 +54,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/catalog" element={<Products />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/order/:id" element={<OrderDetails />} />
             </Routes>
         </main>
         <footer className="footer">
--- frontend/src/Products/data.tsx
+++ frontend/src/Products/data.tsx
@@ -34,5 +34,22 @@ export default{
         rating: 4.5,
         numberOfReviews: 10,
 
+    },
+    {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike', 
+        rating: 4.5,
+        numberOfReviews: 10,
+
+    },{
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike', 
+        rating: 4.5,
+        numberOfReviews: 10,
+
     }]
 }
